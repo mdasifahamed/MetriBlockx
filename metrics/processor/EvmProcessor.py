1b3895855dffa4b1b5e7d7674d285b415c4aa4bc
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-09 02:20:08 +0600

    evm_processor
    evm processor base

---
+++ metrics/processor/EvmProcessor.py
@@ -0,0 +1,14 @@
+from .pools.UniSwapV2Like import UniswapV2LikeMetrics
+from .pools.UniswapV3Like import UniswapV3LikeMetrics
+
+class EvmProcessor(object):
+    """docstring for EvmProcessor."""
+    def __init__(self, arg):
+        pass
+    
+    # create event separator
+    # pass event based on the pool type and event to its specficiclass 
+
+    
+
+
