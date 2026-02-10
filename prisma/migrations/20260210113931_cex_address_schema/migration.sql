da19cf874c84cf08ba879eeca255294098a11e4a
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-10 17:41:42 +0600

    cex_addess
    cex table schema updated and previous migration is deleted

--- prisma/migrations/20260210113931_cex_address_schema/migration.sql
+++ prisma/migrations/20260210113931_cex_address_schema/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "cex_addresses" (
     "id" TEXT NOT NULL,
     "chain_id" INTEGER NOT NULL,
     "cex_name" TEXT NOT NULL,
-    "address" TEXT[],
+    "address" TEXT NOT NULL,
 
     CONSTRAINT "cex_addresses_pkey" PRIMARY KEY ("id")
 );
@@ -12,10 +12,13 @@ CREATE TABLE "cex_addresses" (
 CREATE INDEX "cex_addresses_chain_id_cex_name_idx" ON "cex_addresses"("chain_id", "cex_name");
 
 -- CreateIndex
+CREATE INDEX "cex_addresses_chain_id_address_cex_name_idx" ON "cex_addresses"("chain_id", "address", "cex_name");
+
+-- CreateIndex
 CREATE INDEX "cex_addresses_chain_id_idx" ON "cex_addresses"("chain_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "cex_addresses_chain_id_address_key" ON "cex_addresses"("chain_id", "address");
+CREATE UNIQUE INDEX "cex_addresses_chain_id_cex_name_key" ON "cex_addresses"("chain_id", "cex_name");
 
 -- AddForeignKey
 ALTER TABLE "cex_addresses" ADD CONSTRAINT "cex_addresses_chain_id_fkey" FOREIGN KEY ("chain_id") REFERENCES "chain_config"("chain_id") ON DELETE RESTRICT ON UPDATE CASCADE;
