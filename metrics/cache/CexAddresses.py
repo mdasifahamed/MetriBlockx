e87c64b0cb6013a39b5389b7cb58766c401f729d
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-10 18:17:39 +0600

    cex_address
    global cache class is created for cex address

---
+++ metrics/cache/CexAddresses.py
@@ -0,0 +1,39 @@
+from typing import List, Dict, Optional
+
+
+class CEXAddressCache:
+    _instance = None
+    
+    def __init__(self):
+        self.cexAddress_by_chain = {}    
+    @classmethod    
+    def _getInstance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+    
+    def load(self, cexAddresses: List[Dict]):
+        for cexAddress in cexAddresses:
+            chain_id = cexAddress['chain_id']
+            if chain_id not in self.cexAddress_by_chain:
+                self.cexAddress_by_chain[chain_id] = {}
+            self.cexAddress_by_chain[chain_id][cexAddress['address']] = cexAddress
+                
+    def getAllCexByChainId(self, chainId: int) -> Optional[Dict]:
+        return self.cexAddress_by_chain.get(chainId)
+    
+    def getCexByAddressNChain(self, chainId: int, address: str) -> Optional[Dict]:
+        cexAddress = self.cexAddress_by_chain.get(chainId)
+        if cexAddress is None:
+            return None
+        return cexAddress.get(address)
+    
+    def getCexNameByAddressNChain(self, chainId: int, address: str) -> Optional[str]:
+        cexAddress = self.cexAddress_by_chain.get(chainId)
+        if cexAddress is None:
+            return None
+        entry = cexAddress.get(address)
+        if entry is None:
+            return None
+        return entry['cex_name']
+    
