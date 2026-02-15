d144c6e45c96808ccb2caa12092f23d30abea8bd
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-16 01:22:17 +0600

    stream
    updated the stream as per the new new starem receiver

--- metrics/startStream.py
+++ metrics/startStream.py
@@ -1,6 +1,6 @@
 import asyncio
 import signal
-from .redisStream.ReceiveStreamEvnetBlockRangeEth import ReceiveEthEventBlockRange
+from .redisStream.BaseStreamReceiver import EthereumStreamReceiver, BnbStreamReceiver, PolygonStreamReceiver
 from .redisStream.redisClient import redis_client
 from .db.db_connection import db
 from .db.queries import DBQueries
@@ -9,12 +9,15 @@ from .cache.CexAddresses import CEXAddressCache
 
 
 async def main():
-    receiveEthStream = ReceiveEthEventBlockRange()
+    ethereum_stream_receiver = EthereumStreamReceiver()
+    bnb_stream_receiver = BnbStreamReceiver()
+    polygon_stream_receiver = PolygonStreamReceiver()
+
     loop = asyncio.get_running_loop()
     main_task = asyncio.current_task()
     queries = DBQueries()
-    tokenNPoolCache = TokenNPoolCache()._getInstance()
-    cexAddressCache = CEXAddressCache()._getInstance()
+    token_and_pool_cache = TokenNPoolCache()._getInstance()
+    cex_address_cache = CEXAddressCache()._getInstance()
 
     for sig in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(sig, main_task.cancel)
@@ -23,19 +26,26 @@ async def main():
         await db.connect()
         pools = await queries.getPools()
         tokens = await queries.getTokens()
-        cexAddresses = await queries.getCexAddress()
-        tokenNPoolCache.load(tokens,pools)
-        cexAddressCache.load(cexAddresses=cexAddressCache)
-        # await receiveEthStream.receviveMessage()
+        cex_addresses = await queries.getCexAddress()
+        token_and_pool_cache.load(tokens, pools)
+        cex_address_cache.load(cexAddresses=cex_addresses)
+
+        print("Starting stream receivers for all chains...")
+
+        await asyncio.gather(
+            ethereum_stream_receiver.receiveMessages(),
+            bnb_stream_receiver.receiveMessages(),
+            polygon_stream_receiver.receiveMessages()
+        )
     except asyncio.CancelledError:
         print("Shutting down...")
-    except Exception as e:
-        print(f"Unexpected Error Occurred {e}")
-        raise e
+    except Exception as error:
+        print(f"Unexpected Error Occurred: {error}")
+        raise error
     finally:
         await db.disConnect()
         await redis_client.disconnect()
-        
+
 
 if __name__ == "__main__":
     asyncio.run(main())
