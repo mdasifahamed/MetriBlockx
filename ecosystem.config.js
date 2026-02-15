7a53d604128b89b5b209449f2f9ee82bd194cbb6
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-16 02:17:17 +0600

    pm2
    metrics genartor added and the removed the bnb for the live test and research the purpose

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "data-processors",
+      name: "event-data-processors",
       script: "npx",
       args: "tsx src/startProcessors.ts",
       instances: 1,
@@ -12,11 +12,25 @@ module.exports = {
       },
     },
 
+    // Python Stream Receiver
+    {
+      name: "metrics-genartor",
+      script: "python3",
+      args: "-m metrics.startStream",
+      interpreter: "none",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        PYTHONUNBUFFERED: "1",
+      },
+    },
+
     // Ethereum Worker
     {
       name: "worker-ethereum",
       script: "npx",
-      args: "tsx src/index.ts --chain=ethereum --block=23914924 --targetBlock=23964383",
+      args: "tsx src/index.ts --chain=ethereum --block=24461175 --targetBlock=24461205",
       instances: 1,
       autorestart: true,
       watch: false,
@@ -25,24 +39,10 @@ module.exports = {
       },
     },
 
-    // Polygon Worker
     {
       name: "worker-polygon",
       script: "npx",
-      args: "tsx src/index.ts --chain=polygon --block=70060972 --targetBlock=70866989",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-
-    // BNB Worker
-    {
-      name: "worker-bnb",
-      script: "npx",
-      args: "tsx src/index.ts --chain=bnb --block=79762494 --targetBlock=80021678",
+      args: "tsx src/index.ts --chain=polygon --block=83019390 --targetBlock=83019450",
       instances: 1,
       autorestart: true,
       watch: false,
