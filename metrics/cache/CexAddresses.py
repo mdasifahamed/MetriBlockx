6f3b960482efbdc8b94980e5fc8dec2bf91aad1e
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-16 01:12:32 +0600

    cache
    cex address lowercase issue fixed

--- metrics/cache/CexAddresses.py
+++ metrics/cache/CexAddresses.py
@@ -5,7 +5,8 @@ class CEXAddressCache:
     _instance = None
     
     def __init__(self):
-        self.cexAddress_by_chain = {}    
+        self.cexAddress_by_chain = {}
+    
     @classmethod    
     def _getInstance(cls):
         if cls._instance is None:
@@ -17,7 +18,8 @@ class CEXAddressCache:
             chain_id = cexAddress['chain_id']
             if chain_id not in self.cexAddress_by_chain:
                 self.cexAddress_by_chain[chain_id] = {}
-            self.cexAddress_by_chain[chain_id][cexAddress['address']] = cexAddress
+            address_lower = cexAddress['address'].lower()
+            self.cexAddress_by_chain[chain_id][address_lower] = cexAddress
                 
     def getAllCexByChainId(self, chainId: int) -> Optional[Dict]:
         return self.cexAddress_by_chain.get(chainId)
@@ -26,14 +28,13 @@ class CEXAddressCache:
         cexAddress = self.cexAddress_by_chain.get(chainId)
         if cexAddress is None:
             return None
-        return cexAddress.get(address)
+        return cexAddress.get(address.lower())
     
     def getCexNameByAddressNChain(self, chainId: int, address: str) -> Optional[str]:
         cexAddress = self.cexAddress_by_chain.get(chainId)
         if cexAddress is None:
             return None
-        entry = cexAddress.get(address)
+        entry = cexAddress.get(address.lower())
         if entry is None:
             return None
         return entry['cex_name']
-    
