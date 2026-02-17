08d7d74f868a7cb00a3419832caf333678ff5683
mdasifahamed <mdasifahamed89@yahoo.com>
2026-02-17 12:31:16 +0600

    pm2_config
    only polygon with left blokc from where it crushed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,10 +15,11 @@ module.exports = {
     // Python Stream Receiver
     {
       name: "metrics-genartor",
-      script: "python3",
+      script: "/root/MetriBlockx/metrics/venv/bin/python3",
       args: "-m metrics.startStream",
       interpreter: "none",
       instances: 1,
+      cwd: "/root/MetriBlockx",
       autorestart: true,
       watch: false,
       env: {
@@ -26,23 +27,11 @@ module.exports = {
       },
     },
 
-    // Ethereum Worker
-    {
-      name: "worker-ethereum",
-      script: "npx",
-      args: "tsx src/index.ts --chain=ethereum --block=24461175 --targetBlock=24461205",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-
+    // Polygon Worker
     {
       name: "worker-polygon",
       script: "npx",
-      args: "tsx src/index.ts --chain=polygon --block=83019390 --targetBlock=83019450",
+      args: "tsx src/index.ts --chain=polygon --block=82858373 --targetBlock=82890571",
       instances: 1,
       autorestart: true,
       watch: false,
